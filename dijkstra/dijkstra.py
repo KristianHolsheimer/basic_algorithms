5c076bd643512da444719138521eb78ce92d0b32
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2014-11-23 14:02:01 +0100

    added comments to main while-loop in dijkstra.py

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -11,15 +11,19 @@ with open('simple.txt', 'r') as f:
 ####  LEGEND:  #################################################################
 ## 
 ## n = current node
+##		format: integer
 ## 
 ## g = graph, adjacency dict
-##       {node: {adjacent_node: length, ... }, ... }
+##      format: {node: {adjacent_node: length, ... }, ... }
 ##
 ## l = dict of lengths from starting node
-##       {node: length, ... }
+##      format: {node: length, ... }
 ##
 ## e = edges connecting seen and unseen nodes (with their respective distances)
-##       {(node_seen, node_unseen): length, ... }
+##      format: {(node_seen, node_unseen): length, ... }
+##
+## emin = edge in e with minimal path length 
+##		format: (node_seen, node_unseen)
 ##
 ################################################################################
 
@@ -30,14 +34,14 @@ for i in g[n]:
 
 # the main loop
 while e:
-    emin = min(e, key=e.get)
-    n = emin[1]
-    l[n] = e.pop(emin)
+    emin = min(e, key=e.get)                        # get minimum edge
+    n = emin[1]                                     # set current node accordingly
+    l[n] = e.pop(emin)								# set corresponding path length (and remove edge from e)
 
-    for i in g[n]:
-        e[(n,i)] = g[n][i] + l[n]
+    for i in g[n]:									# iterate over all nodes adjacent to new node n
+        e[(n,i)] = g[n][i] + l[n]					# add new cross edges to e
 
-    e = {k:v for k,v in e.items() if k[1] not in l}
+    e = {k:v for k,v in e.items() if k[1] != n} 	# remove redundant edges connecting two seen nodes
 
 
 
