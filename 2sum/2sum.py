c27357f55c715a0bf7109069b30160dc988b54c6
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2014-12-18 18:36:43 +0100

    cleaned up 2sum.py

--- 2sum/2sum.py
+++ 2sum/2sum.py
@@ -1,20 +1,24 @@
-import time
+#!/usr/bin/pypy3
 
+# load array A as a Python set:
 with open('2sum_example.txt', 'r') as f:
-    A = set([])
+    A = set()
     for n in f:
         A.add(int(n))
 
+
+# initialize the set of t's:
 ts = set()
 
-print(len(A))
-
+# for each element in A, check which t's satisfy "t-x in A"
 while A:
     x = A.pop()
-    ts.update([t for t in range(-10000,10001) if t-x in A])
-
-print('count = {}'.format(len(ts)))
+    ts.update(t for t in range(-10000,10001) if t-x in A)
 
 
-## expected output from "2sum_example.txt":
-## count = 5
+# count the number of such t's:
+print(len(ts))
+
+
+# expected output from "2sum_example.txt":
+# 5
